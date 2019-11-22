02a704cbc9c36444b797fb68352215b1d617dad5
gersoncjlima <gersoncjlima@gmail.com>
2019-11-22 03:53:55 -0300

    criação de index.html e pasta js com arquivo main.js.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//var time = new Date();
+//console.log(time.getHours() + ":" + time.getMinutes() +":" + time.getSecondes());
+
+et d = new Date();
+document.body.innerHTML = "<h1>Time right now is: " + d.getHours() + 
+":" + d.getMinutes() + ":" + d.getSecondes()</h1>"
